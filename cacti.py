e533398cdb1338dce145c3874c4b885acdd76b16
Brobo1 <armanahz95@gmail.com>
2025-02-24 19:25:35 +0100

    refactor: fix indentation for improved readability in cacti.py and initMaze.py

--- cacti.py
+++ cacti.py
@@ -3,24 +3,24 @@ from planter import *
 from pumpkins import *
 
 def waterTile():
-    if get_water() < 0.90:
-        use_item(Items.Water)
+	if get_water() < 0.90:
+		use_item(Items.Water)
 
 
 resetDrone()
 
 while True: 
-    for i in range(get_world_size()):
-        for j in range(get_world_size()):
-            waterTile()
-            tiller(Grounds.Soil)
-            
-            if can_harvest():
-                harvest()
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			waterTile()
+			tiller(Grounds.Soil)
+			
+			if can_harvest():
+					harvest()
 
-            if get_entity_type() != Entities.Cactus:
-                plant(Entities.Cactus)
+			if get_entity_type() != Entities.Cactus:
+					plant(Entities.Cactus)
 
-            goTo(i,j)
+			goTo(i,j)
 
 
--- initMaze.py
+++ initMaze.py
@@ -2,9 +2,9 @@ from utils import *
 
 
 while True: 
-    for i in range(get_world_size()):
-        for j in range(get_world_size()):
-            goTo(i,j)
-            tiller(Grounds.Grassland)
-            if can_harvest():
-                harvest()
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			goTo(i,j)
+			tiller(Grounds.Grassland)
+			if can_harvest():
+				harvest()
