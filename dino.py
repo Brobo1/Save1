ca74c4cee1e5e5b78fb8823d582542fdcefcc6b4
Brobo1 <armanahz95@gmail.com>
2025-02-26 20:10:17 +0100

    feat: enhance dinoBones function with lap-based movement logic and optimize apple collection

--- dino.py
+++ dino.py
@@ -6,8 +6,12 @@ def dinoBones():
 	vDir = [North, South]
 	turn = True
 	turnIndex = 0
+	lapIndex = 0
+	gridType = get_world_size() % 2 == 0
 	size = get_world_size()-1
 	apples = []
+
+	quick_print(gridType)
 
 	change_hat(Hats.Dinosaur_Hat)
 	goTo(0,0)
@@ -16,14 +20,29 @@ def dinoBones():
 	while len(apples) <= 32:
 
 		quick_print(len(apples))
-		while get_pos_y() < size and move(vDir[0]):
-			addApples(apples)
+
+		if gridType:
+			while get_pos_y() < size and move(vDir[0]):
+				addApples(apples)
+		else:
+			if lapIndex % 2 == 0:
+				while get_pos_y() < size-1 and move(vDir[0]):
+					addApples(apples)
+			else: 
+				while get_pos_y() < size and move(vDir[0]):
+					addApples(apples)
+			lapIndex += 1
 
 		while get_pos_x() < size and move(hDir[0]):
 			addApples(apples)
 
-		move(vDir[1])
-		addApples(apples)
+		if lapIndex % 2 == 0:
+			move(vDir[1])
+			addApples(apples)
+		else:
+			move(vDir[1])
+			move(vDir[1])
+			addApples(apples)
 
 		while get_pos_x() != 0 and get_pos_y() !=0 and len(apples) <= 32:
 			quick_print(len(apples))
@@ -49,41 +68,8 @@ def addApples(apples):
 		apples.append(measure())
 
 
-# change_hat(Hats.Dinosaur_Hat)
 
 while True:
 	dinoBones()
 
 
-
-
-
-
-# def dinoBones():
-# 	hDir = [East, West]
-# 	vDir = [North, South]
-# 	turn = True
-# 	turnIndex = 0
-
-# 	while True:
-
-# 		while move(vDir[turnIndex % 2]):
-# 			pass
-
-# 		if turn:
-# 			move(hDir[turnIndex % 2])
-# 			turn = not turn
-# 		else:
-# 			move(hDir[(turnIndex+1) % 2])
-# 			turn = not turn
-
-# 		while move(vDir[(turnIndex+1) % 2]):
-# 			pass
-
-# 		turn = not turn
-
-# 		if (get_pos_x() == get_world_size()-1 and get_pos_y() == get_world_size()-1
-# 			or get_pos_x() == 0 and get_pos_y() == 0
-# 			or get_pos_x() == get_world_size()-1 and get_pos_y() == 0
-# 			or get_pos_x() == 0 and get_pos_y() == get_world_size()-1):
-			# turnIndex += 1 
