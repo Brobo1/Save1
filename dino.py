654fc4e12e2240063608ebb89b0d5c4e4df5dd95
Brobo1 <armanahz95@gmail.com>
2025-02-26 18:13:19 +0100

    feat: implement dinoBones function for enhanced movement logic and hat changes

--- dino.py
+++ dino.py
@@ -1,6 +1,55 @@
 from utils import *
 
 
+def dinoBones():
+	hDir = [East, West]
+	vDir = [North, South]
+	turn = True
+	turnIndex = 0
+	size = get_world_size()-1
+
+	change_hat(Hats.Dinosaur_Hat)
+	goTo(0,0)
+	change_hat(Hats.Dinosaur_Hat)
+
+	while True:
+		
+
+		while get_pos_y() < size:
+			move(vDir[0])
+
+		while get_pos_x() < size:
+			move(hDir[0])
+
+		move(vDir[1])
+
+		while get_pos_x() != 0 and get_pos_y() !=0:
+			
+			while get_pos_x() > 1: 
+				move(hDir[1])
+
+			move(vDir[1])
+
+			while get_pos_x() < size:
+				move(hDir[0])
+
+			move(vDir[1])
+
+		while get_pos_x() != 0:
+			move(hDir[1])
+
+
+
+
+
+# change_hat(Hats.Dinosaur_Hat)
+dinoBones()
+
+
+
+
+
+
 # def dinoBones():
 # 	hDir = [East, West]
 # 	vDir = [North, South]
@@ -29,38 +78,3 @@ from utils import *
 # 			or get_pos_x() == get_world_size()-1 and get_pos_y() == 0
 # 			or get_pos_x() == 0 and get_pos_y() == get_world_size()-1):
 			# turnIndex += 1 
-
-def dinoBones():
-	hDir = [East, West]
-	vDir = [North, South]
-	turn = True
-	turnIndex = 0
-	size = get_world_size()-1
-
-
-	change_hat(Hats.Dinosaur_Hat)
-	goTo(0,0)
-	change_hat(Hats.Dinosaur_Hat)
-
-	while get_pos_y() != size:
-		move(vDir[turnIndex])
-		pass
-
-	while move(hDir[turnIndex]):
-		pass 
-
-	while get_pos_x() == size and get_pos_y() == size:
-		move(vDir[(turnIndex+1)%2])
-	
-	while get_pos_x() > 1 and get_pos_y() == size-1:
-		move(hDir[(turnIndex+1)%2])
-	
-	while True:
-		while move(vDir[(turnIndex+1)%2]):
-			pass
-
-		while  
-
-# change_hat(Hats.Dinosaur_Hat)
-dinoBones()
-
