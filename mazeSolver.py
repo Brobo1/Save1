c67b7fcbd56aefd12fb46b4d3301eaa668a341e4
Brobo1 <armanahz95@gmail.com>
2025-02-26 18:35:25 +0100

    feat: add mazeSolver module with maze creation and pathfinding functionality

---
+++ mazeSolver.py
@@ -0,0 +1,28 @@
+
+def makeMaze():
+	harvest()
+	plant(Entities.Bush)
+	use_item(Items.Weird_Substance, get_world_size())
+
+def pathFinder():
+	dirs = [North, East, South, West]
+	curDirIndex = 0
+
+	while get_entity_type() != Entities.Treasure:
+		dirIndex = (curDirIndex-1)%4
+
+		if move(dirs[dirIndex]):
+			curDirIndex = dirIndex
+		elif move(dirs[curDirIndex]):
+			pass
+		else:
+			curDirIndex = (curDirIndex+1)%4
+	
+	if get_entity_type() == Entities.Treasure:
+		harvest()
+
+while True:
+	makeMaze()
+	pathFinder()
+
+# goTo(0,0)
