77b6c96406aa039cc5e728b2f7109ebdc6829dc0
Brobo1 <armanahz95@gmail.com>
2025-02-28 15:25:35 +0100

    feat: enhance hayRun logic with improved harvesting and planting mechanics; add tilling for soil ground type

--- hayRun.py
+++ hayRun.py
@@ -9,31 +9,30 @@ while num_items(Items.Hay) <= 100000:
 
 	if (get_entity_type() != Entities.Grass):
 		harvest()
+		if get_ground_type() == Grounds.Soil:
+			till()
 
-	grow, (x1, y1) = get_companion()
-
-	while grow == Entities.Carrot:
-		harvest()
+	if can_harvest():
 		grow, (x1, y1) = get_companion()
+		if (grow == Entities.Carrot):
+			if num_items(Items.Wood) >= 12:
+				goTo(x1,y1)
+				tiller(Grounds.Soil)
+				plant(grow)
+			else:	
+				harvest()
+		else:
+			goTo(x1,y1)
+			harvest()
+			plant(grow)
 
-	goTo(x1,y1)
-	harvest()
-	plant(grow)
+		goTo(x2,y2)
+		harvest()
 
-
-	goTo(x2,y2)
-	
-	# while not can_harvest() and get_entity_type() == Entities.Grass:
-	# 	if num_items(Items.Fertilizer) > 0:
-	# 		use_item(Items.Fertilizer)
-	# 	pass
-		
 	if y2 >=9:
 		x2 = (x2 + 1) % 10
 	y2 = (y2 + 1) % 10
 
-	if can_harvest():
-		harvest()
 	
 
 	
