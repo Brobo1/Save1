639fd4b4fc966af07919a2cc25374b618d4caf99
Brobo1 <armanahz95@gmail.com>
2025-02-28 08:27:45 +0100

    feat: update hayRun simulation logic and adjust power parameter for improved performance

--- hayRun.py
+++ hayRun.py
@@ -1,11 +1,22 @@
 from utils import *
 
-
+x2,y2 = 0, 0
 while num_items(Items.Hay) <= 100000:
-	for i in range(get_world_size()):
-		grow, (x,y) = get_companion()
-		quick_print(grow, x,y)
+	grow, (x1,y1) = get_companion()
+
+	if (grow != Entities.Carrot and get_entity_type() == Entities.Grass ):
+		goTo(x1,y1)
+		plant(grow)
+		goTo(x2,y2)
+	else:
+		goTo(x2,y2)
+		
+	
+	
+
+		quick_print(y2)
+	
+	if can_harvest():
+		y2 = (y2 +1) % 9
 		harvest()
-		move(North)
-	move(East)
 
