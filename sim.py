6a483dac22313db296008f8741195e6e4a288b7c
Brobo1 <armanahz95@gmail.com>
2025-03-04 22:37:19 +0100

    feat: update leaderboard and simulation logic for woodRun; adjust parameters for improved functionality

--- sim.py
+++ sim.py
@@ -9,14 +9,25 @@
 # 		0.1
 # 	)
 # )
+# quick_print(
+# 	simulate(
+# 		"cactiRun",
+# 		Unlocks,
+# 		{Items.Pumpkin : 1000000,
+# 		Items.Power: 1000000},
+# 		{},
+# 		-1,
+# 		1
+# 	)
+# )
+
 quick_print(
 	simulate(
-		"cactiRun",
+		"woodRun",
 		Unlocks,
-		{Items.Pumpkin : 1000000,
-		Items.Power: 1000000},
+		{Items.Power: 1000000},
 		{},
 		-1,
-		1
+		0.5
 	)
 )
